453a53297785d1c7392f1d43d51109d5bd7dfaff
marwasyr <marwaalahdab14@gmail.com>
2023-07-08 13:19:16 +0300

    Lab8

---
+++ Phils_Lending_Library/Backpack.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static System.Reflection.Metadata.BlobBuilder;
+
+namespace Phils_Lending_Library
+{
+    public class Backpack<T> : IBag<T>
+    {
+
+        private List<T> items;
+
+        public Backpack()
+        {
+            items = new List<T>();
+        }
+
+
+     
+        /// <summary>
+        /// Add an item to the bag. <c>null</c> items are ignored.
+        /// </summary>
+        public void Pack(T item)
+    {
+            items.Add(item);
+    }
+
+    /// <summary>
+    /// Remove the item from the bag at the given index.
+    /// </summary>
+    /// <returns>The item that was removed.</returns>
+       public T Unpack(int index)
+        {
+
+            T item = items[index];
+            items.Remove(item);
+            return item;
+
+        }
+
+
+
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+
+    }
+}
+
---
+++ Phils_Lending_Library/Library.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Drawing;
+
+namespace Phils_Lending_Library
+{
+    public class Library: ILibrary
+    {
+        //Since books need to be borrowed by Title, use a private Dictionary<string, Book> for storage.
+
+        private Dictionary<string, Book> books;
+
+        public Library()
+        {
+            books = new Dictionary<string, Book>();
+        }
+
+
+
+        public int Count => books.Count;
+
+
+
+
+        public void Add(string title, string firstName, string lastName, int numberOfPages)
+        {
+            Book book = new Book(title, firstName, lastName, numberOfPages);
+            books.Add(title, book);
+        }
+
+
+        public Book Borrow(string title)
+        {
+
+            foreach(KeyValuePair<string, Book> book in books)
+            {
+                if(book.Key == title)
+                {
+                    books.Remove(book.Key);
+                    return book.Value;
+                }
+            }
+
+            return null;
+
+        }
+
+
+        public void Return(Book book)
+        {
+           
+            books[book.Title] = book;
+            // add the value "book" at the key "book.Title"
+
+        }
+
+        public IEnumerator<Book> GetEnumerator()      //enables iteration over a collection of Book objects
+
+        {
+            return books.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
+
